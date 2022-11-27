8e1cf7c923f55f3a1d13ab7072d33e357fa2cd96
harshitadatra <57798468+harshitadatra@users.noreply.github.com>
2022-11-27 12:50:10 +0530

    removed comments

--- src/App.js
+++ src/App.js
@@ -1,20 +1,14 @@
 import "./App.css";
-import { ProductListingPage } from "./components/ProductListing/ProductListingPage";
-import { Home } from "./components/Home/Home";
+
 import { RouterPath } from "./routes";
-import { LoginPage } from "./components/Login/Login";
-import { WishlistPage } from "./components/Wishlist/WishlistPage";
-import { MockAPI } from "./mockMan";
+
 function App() {
   return (
     <div className="App">
       <RouterPath>
-        {/* <LoginPage />
-        <Home />
-        <ProductListingPage />
-        <WishlistPage /> */}
+    
       </RouterPath>
-      {/* <MockAPI/>  */}
+    
     </div>
   );
 }
